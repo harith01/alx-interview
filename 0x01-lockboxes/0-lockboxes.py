6ad1a6a63f44f63fe82197a2d4db396079db6607
harith01 <harithyusuph@gmail.com>
2023-10-19 21:39:18 +0100

    Lockbox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,11 +8,12 @@ def canUnlockAll(boxes):
     if len(boxes[0]) == 0:
         return False
 
-    keys = []
+    checked = []
+    to_be_checked = boxes[0]
 
-    for i in range(len(boxes)):
-        for key in boxes[i]:
-            if key != i and key < len(boxes):
-                keys.append(key)
-    keys = set(keys)
-    return (len(keys) == len(boxes) or len(keys) == len(boxes) - 1)
+    for key in to_be_checked:
+        if key not in checked:
+            to_be_checked.extend(boxes[key])
+            checked.append(key)
+
+    return (len(checked) == len(boxes) or len(checked) == len(boxes) - 1)
