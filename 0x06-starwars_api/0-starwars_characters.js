0cbaf48433691b7fdba635c7569e81027252b9f8
harith01 <harithyusuph@gmail.com>
2023-11-07 15:46:04 +0100

    Task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,18 +2,27 @@
 
 const request = require('request');
 
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const name = JSON.parse(body).name;
+      console.log(name);
+      sendRequest(characterList, index + 1);
+    }
+  });
+}
+
 request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (error, response, body) => {
   if (error) {
     console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    sendRequest(characters, 0);
   }
-  const characters = JSON.parse(body).characters;
-  characters.forEach((character) => {
-    request(character, (error, response, body) => {
-      if (error) {
-        console.log(error);
-      }
-      const name = JSON.parse(body).name;
-      console.log(name);
-    });
-  });
 });
