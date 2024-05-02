30ad8c5eb14b3a2cce26bba0b53ac738613f996a
harith01 <harithyusuph@gmail.com>
2024-05-02 17:56:57 +0100

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,14 +8,13 @@ def canUnlockAll(boxes):
     """A method that determines if all
     boxes can be unlocked"""
 
-    unlocked_boxes = [0]
-    for box_id, box in enumerate(boxes):
+    un_boxes = [0]
+    for b_id, box in enumerate(boxes):
         if not box:
             continue
         for key in box:
-            if key < len(boxes) and key not in unlocked_boxes
-            and key != box_id:
-                unlocked_boxes.append(key)
-    if len(boxes) == len(unlocked_boxes):
+            if key < len(boxes) and key not in un_boxes and key != b_id:
+                un_boxes.append(key)
+    if len(boxes) == len(un_boxes):
         return True
     return False
