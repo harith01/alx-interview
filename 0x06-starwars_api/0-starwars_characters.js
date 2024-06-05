ceb00dc90667fbb20cc953285bf6eb21699af28f
harith01 <harithyusuph@gmail.com>
2024-06-05 08:43:00 +0100

    Task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,14 +3,20 @@
 const request = require('request')
 const process = require('process')
 
-let url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
-request(url, (error, response, body) => {
-  if (error) throw new Error(error);
-  let characters = JSON.parse(body).characters;
-  characters.map((character) => {
-    request(character, (error, response, body) => {
-      if (error) throw new Error(error);
-      console.log(JSON.parse(body).name)
-    })
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+if (process.argv.length > 2) {
+  request(url, (error, _, body) => {
+    if (error) throw new Error(error);
+    const characters = JSON.parse(body).characters;
+    const names = characters.map(character => new Promise((resolve, reject) => {
+      request(character, (error, _, body) => {
+        if (error) throw new Error(error);
+	resolve(JSON.parse(body).name);
+      })
+    }))
+    Promise.all(names)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   })
-  });
+}
